d169ffd65ca230dcce7e36b8a0caba925afeb966
fellon-dnb <fellon@ancevt.com>
2025-12-15 15:22:43 +0300

    init commit

---
+++ src/main/java/com/fellon/streamapi/StreamCollectorsExample.java
@@ -0,0 +1,20 @@
+package com.fellon.streamapi;
+
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+public class StreamCollectorsExample {
+    public static void main(String[] args) {
+        List<Order> orders = List.of(
+                new Order("Laptop", 1200.0),
+                new Order("Smartphone", 800.0),
+                new Order("Laptop", 1500.0),
+                new Order("Tablet", 500.0),
+                new Order("Smartphone", 900.0)
+        );
+
+    }
+
+
+}
